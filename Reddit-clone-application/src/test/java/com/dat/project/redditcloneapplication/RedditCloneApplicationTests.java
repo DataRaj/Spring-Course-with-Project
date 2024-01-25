93fc978bdcab3455a6a8d16202d97f3f65200111
Dattaraj Dhadas <dhadas808@yahoo.com>
2024-01-25 20:28:39 +0530

    pushing the main files

---
+++ Reddit-clone-application/src/test/java/com/dat/project/redditcloneapplication/RedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dat.project.redditcloneapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
